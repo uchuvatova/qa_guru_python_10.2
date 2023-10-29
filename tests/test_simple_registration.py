90adb1d820c24de7c162d90a623479d8c38fe095
uchuvatova <109352814+uchuvatova@users.noreply.github.com>
2023-10-29 17:43:40 +0300

    данные и тесты в ветке high-level-step-objects

--- tests/test_simple_registration.py
+++ tests/test_simple_registration.py
@@ -4,20 +4,20 @@ from demoqa_tests.pages.shortRegistrationPage import ShortRegistrationPage
 
 def test_success_simple_registration():
     ira = User(
-        first_name = 'Ira',
-        last_name = 'Uchuvatova',
-        email = 'example@mail.ru',
-        gender = Gender.female.value,
-        phone = '1234567890',
-        year_of_birth = '1986',
-        month_of_birth = 'April',
-        day_of_birth = '26',
-        subjects = 'Maths',
-        hobby = 'Reading',
-        photo = '1.png',
-        current_address = 'Sunstreet, 28',
-        state = 'NCR',
-        city = 'Delhi'
+        first_name='Ira',
+        last_name='Uchuvatova',
+        email='example@mail.ru',
+        gender=Gender.female.value,
+        phone='1234567890',
+        year_of_birth='1986',
+        month_of_birth='April',
+        day_of_birth='26',
+        subjects='Maths',
+        hobby='Reading',
+        photo='1.png',
+        current_address='Sunstreet, 28',
+        state='NCR',
+        city='Delhi'
     )
     registration_page = ShortRegistrationPage()
     registration_page.open()
