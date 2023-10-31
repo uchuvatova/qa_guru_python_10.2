6ccf88904cc571717c76d3cfdb14eada51726664
uchuvatova <109352814+uchuvatova@users.noreply.github.com>
2023-10-31 10:22:42 +0300

    изменен путь к allure-results

--- conftest.py
+++ conftest.py
@@ -30,7 +30,7 @@ def setup_browser(request):
         options=options
     )
 
-    browser = Browser(Config(driver))
+    browser = Browser(Config())
     yield browser
 
     attach.add_screenshot(browser)
